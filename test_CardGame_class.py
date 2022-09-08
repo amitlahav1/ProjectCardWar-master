34b28a24a1412363ee282195e7392d4fc45ffc07
amitla100 <amit.lahav100@gmail.com>
2022-09-08 23:44:57 +0300

    our project

--- test_CardGame_class.py
+++ test_CardGame_class.py
@@ -1,7 +1,4 @@
-from unittest import TestCase
-
-import Card_Class
-import Player_class
+from unittest import TestCase, mock
 from DeckOfCards_Class import DeckOfCards
 from Player_class import Player
 from unittest.mock import patch
@@ -12,52 +9,61 @@ class TestCardgame(TestCase):
 
     def test_setUp(self):
         self.deck_cards = DeckOfCards()
+        self.game = Cardgame("amit", "elior", 26)
 
-#test init valid. the game get valid argument
+    # test init valid. the game get valid argument
     def test_init_valid_1(self):
-        self.game = Cardgame("amit","elior",26)
+        self.game = Cardgame("amit", "elior", 26)
 
-        self.assertEqual(len(self.game.player1.cards_player_list),26)
-        self.assertEqual(self.game.player1.name,"amit")
-        self.assertEqual(self.game.player2.name,"elior")
+        self.assertEqual(len(self.game.player1.cards_player_list), 26)
+        self.assertEqual(self.game.player1.name, "amit")
+        self.assertEqual(self.game.player2.name, "elior")
 
-#test init valid.the game get valid argument
+    # test init valid.the game get valid argument
     def test_init_valid_2(self):
-        self.game = Cardgame("","")
-        self.assertEqual(self.game.player1.name,"player1")
-        self.assertEqual(self.game.player2.name,"player1")
+        self.game = Cardgame("", "")
+        self.assertEqual(self.game.player1.name, "player1")
+        self.assertEqual(self.game.player2.name, "player1")
 
-# player_cards_amount>26. defult =26. the game get the correctly arguments
+    # player_cards_amount>26. defult =26. the game get the correctly arguments
     def test_init_invalid_1(self):
-        self.game = Cardgame("", "",27)
-        self.assertEqual(len(self.game.player1.cards_player_list),26)
+        self.game = Cardgame("", "", 27)
+        self.assertEqual(len(self.game.player1.cards_player_list), 26)
 
-# test to amount card is 15, player 1 =15 cards.the game get the correctly arguments
+    # test to amount card is 15, player 1 =15 cards.the game get the correctly arguments
     def test_init_valid_3(self):
         self.game = Cardgame("", "", 15)
         self.assertEqual(len(self.game.player1.cards_player_list), 15)
-        self.assertEqual(len(self.game.player2.cards_player_list),15)
+        self.assertEqual(len(self.game.player2.cards_player_list), 15)
 
- #player_cards_amount<10. defult =26. the game get the correctly arguments
+    # player_cards_amount<10. defult =26. the game get the correctly arguments
     def test_init_invalid_2(self):
-         self.game = Cardgame("", "",5)
-         self.assertEqual(len(self.game.player1.cards_player_list), 26)
+        self.game = Cardgame("", "", 5)
+        self.assertEqual(len(self.game.player1.cards_player_list), 26)
 
-#after the cards are dealt, the players' decks should be equal
+    # after the cards are dealt, the players' decks should be equal
     def test_new_game_valid_1(self):
-        self.game = Cardgame("amit","elior",26)
-        p1_len=len(self.game.player1.cards_player_list)
-        p2_len=len(self.game.player2.cards_player_list)
+        self.game = Cardgame("amit", "elior", 26)
+        p1_len = len(self.game.player1.cards_player_list)
+        p2_len = len(self.game.player2.cards_player_list)
         self.assertTrue(p1_len == p2_len)
 
-# after the cards are dealt, the players' decks should be equal
+    # after the cards are dealt, the players' decks should be equal
     def test_new_game_valid_1(self):
         self.game = Cardgame("amit", "elior", 12)
         p1_len = len(self.game.player1.cards_player_list)
         p2_len = len(self.game.player2.cards_player_list)
         self.assertTrue(p1_len == p2_len)
 
+    # the len of deck id equal. valid
+    def test_get_winner_equal_valid_1(self):
+        self.p1 = Player("amit")
+        self.p2 = Player("elior")
+        self.game = Cardgame(self.p1, self.p2)
+        self.assertTrue(self.game.get_winner(), "none")
 
-    def test_get_winner(self):
-        with patch () as mock_card:
-            mock_card.return_value = card_1
+    # player 1 get one card. player2 zero
+    def test_get_winner_player1_bigger_valid_2(self):
+        self.game.player1.cards_player_list = ["Spade", 3]
+        self.assertNotEqual(self.game.get_winner(), self.game.player2)
+
--- test_DeckOfCards_Class.py
+++ test_DeckOfCards_Class.py
@@ -8,48 +8,47 @@ class TestDeckOfCards(TestCase):
     def setUp(self):
         self.deck_valid = DeckOfCards()
 
-
-# test that  len of deck is 52.valid
+    # test that  len of deck is 52.valid
     def test_deck_len_valid_1(self):
-        self.assertEqual(len(self.deck_valid.deck52),52)
+        self.assertEqual(len(self.deck_valid.deck52), 52)
 
-# test that len of deck is 53.invalid
+    # test that len of deck is 53.invalid
     def test_deck_len_invalid_1(self):
-            self.assertNotEqual(len(self.deck_valid.deck52), 53)
+        self.assertNotEqual(len(self.deck_valid.deck52), 53)
 
-# test that len of deck is 51.invalid
+    # test that len of deck is 51.invalid
     def test_deck_len_invalid_2(self):
-            self.assertNotEqual(len(self.deck_valid.deck52), 51)
+        self.assertNotEqual(len(self.deck_valid.deck52), 51)
 
-# test to check that only 1 type of card in the deck. no dubles.
+    # test to check that only 1 type of card in the deck. no dubles.
     def test_deck_one_card_valid_1(self):
         for i in self.deck_valid.deck52:
             self.assertEqual(self.deck_valid.deck52.count(i), 1)
 
-# test to check that have dubles card in deck. invalid.
+    # test to check that have dubles card in deck. invalid.
     def test_deck_duble_card_invalid_1(self):
         for i in self.deck_valid.deck52:
             self.assertNotEqual(self.deck_valid.deck52.count(i), 2)
 
-#test valid.card ace need to be in deck as value 14.
+    # test valid.card ace need to be in deck as value 14.
     def test_ace_power_invalid_1(self):
         card_ace = Card("Spade", 14)
         card_ace_not = Card("Spade", 1)
-        self.assertNotIn(card_ace_not,self.deck_valid.deck52)
-        self.assertIn(card_ace,self.deck_valid.deck52)
+        self.assertNotIn(card_ace_not, self.deck_valid.deck52)
+        self.assertIn(card_ace, self.deck_valid.deck52)
 
-#test that deck after the shuffle not same
+    # test that deck after the shuffle not same
 
     def test_cards_shuffle_valid_1(self):
-        self.assertNotEqual(self.deck_valid.cards_shuffle(),self.deck_valid)
+        self.assertNotEqual(self.deck_valid.cards_shuffle(), self.deck_valid)
 
-#take out from the deck  card and check that the card dont exist in the deck.valid
+    # take out from the deck  card and check that the card dont exist in the deck.valid
     def test_del_one_card_valid_1(self):
-        self.assertNotIn(self.deck_valid.del_one(),self.deck_valid.deck52)
+        self.assertNotIn(self.deck_valid.del_one(), self.deck_valid.deck52)
 
-#chek if the function del return the card. valid
+    # chek if the function del return the card. valid
     def test_return_card_del_valid_2(self):
         card_1 = Card("Spade", 5)
-        with patch ('DeckOfCards_Class.DeckOfCards.del_one') as mock_card:
+        with patch('DeckOfCards_Class.DeckOfCards.del_one') as mock_card:
             mock_card.return_value = card_1
             self.assertEqual(self.deck_valid.del_one(), card_1)
